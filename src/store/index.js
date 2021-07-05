d0d8a54d1ba5a2caa3962f4887d08c65df7aa530
Manoj Kumar <manojkumardasari111@gmail.com>
2021-07-06 01:31:49 +0530

    backend

--- src/store/index.js
+++ src/store/index.js
@@ -1,44 +1,107 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 
+import feathers from 'feathers-client';
+import io from 'socket.io-client'
 Vue.use(Vuex)
+
+
+const host = 'http://localhost:3030';
+const socket = io(host);
+
+export const app = feathers()
+  .configure(feathers.socketio(socket))
+  .configure(feathers.hooks());
+
+Vue.prototype.$feathersapp = app;
+
+
+
+const todoService = app.service('tasks');
+// todoService is an event emitter
+todoService.on('created', todo => console.log('Created todo', todo));
+
 
 export default new Vuex.Store({ 
   state: {
         newTaskName:'',
-        tasks:[
-          
-        ]
+        tasks:[]
   },
   mutations: {
     deleteTask(state,id)
       {
+
+        let taskt = state.tasks.filter(task => task.id === id)
+        console.log(taskt)
         state.tasks = state.tasks.filter(task => task.id !== id)
-        window.localStorage.setItem('tasks',JSON.stringify(state.tasks))
+
+
+        app.service('tasks').remove(id).then((result)=>{
+          console.log(result)
+        }).catch((error)=>{
+          console.log(error)
+        })
         
       },
       
       addTask(state,newTaskName)
       {
           if (state.newTaskName === '') return 
-          let newTask = {
-          id : Date.now(),
-          name : state.newTaskName,
+          var c = state.newTaskName
+          
+        
+        app.service('tasks').create({
+          tasks:c,
           done:false
-        }
-        state.tasks.push(newTask)
-        state.newTaskName=''
-        window.localStorage.setItem('tasks',JSON.stringify(state.tasks))
+        }).then((result)=>{
+          console.log(result)
+          let newTask = {
+            id : result._id,
+            name : c,
+            done:false
+          }
+          state.tasks.push(newTask)
+          state.newTaskName=''
+
+        }).catch((error)=>{
+          console.log(error)
+        })
+
+        
       },
       getUnits: function(state) {
-        state.tasks = JSON.parse(window.localStorage.getItem('tasks'))
-        if(state.tasks == null)
-        window.localStorage.setItem('tasks',JSON.stringify([]))    
+        state.tasks = []
+        var dbTasks = []
+        app.service('tasks').find().then((result)=>{
+          dbTasks = result
+          console.log(dbTasks.data,'tdfghj')
+          for (let i = 0; i < dbTasks.data.length; i++) {
+            let newTask = {
+              id : dbTasks.data[i]._id,
+              name : dbTasks.data[i].tasks,
+              done:dbTasks.data[i].done
+            }
+            state.tasks.push(newTask)
+            console.log(state.tasks)
+          }
+        }).catch((error)=>{
+          console.log(error)
+        })  
+        
       },
       doneTask(state,id){
         let task = state.tasks.filter(task => task.id === id)[0]
         task.done = !task.done
-        window.localStorage.setItem('tasks',JSON.stringify(state.tasks))
+        
+        app.service('tasks').update(task.id,{
+          tasks:task.name,
+          done:task.done
+        }).then((result)=>{
+          console.log(result)
+        }).catch((error)=>{
+          console.log(error)
+        })
+        
       }
   },
   actions: {
