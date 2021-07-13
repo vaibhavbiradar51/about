4ec631e52eddceb0e6bc8b80f355af9f6d05d9c4
vaibhavbiradar51 <biradarvaibhav50@gmail.com>
2021-07-13 19:17:27 +0530

    first commit

--- src/main.js
+++ src/main.js
@@ -1,14 +1,32 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
-import vuetify from './plugins/vuetify'
+import Vue from 'vue';
+import singleSpaVue from 'single-spa-vue';
 
-Vue.config.productionTip = false
+import App from './App.vue';
+import router from './router';
 
-new Vue({
-  router,
-  store,
-  vuetify,
-  render: h => h(App)
-}).$mount('#app')
+Vue.config.productionTip = false;
+
+const vueLifecycles = singleSpaVue({
+  Vue,
+  appOptions: {
+    render(h) {
+      return h(App, {
+        props: {
+          // single-spa props are available on the "this" object. Forward them to your component as needed.
+          // https://single-spa.js.org/docs/building-applications#lifecyle-props
+          // if you uncomment these, remember to add matching prop definitions for them in your App.vue file.
+          /*
+          name: this.name,
+          mountParcel: this.mountParcel,
+          singleSpa: this.singleSpa,
+          */
+        },
+      });
+    },
+    router,
+  },
+});
+
+export const bootstrap = vueLifecycles.bootstrap;
+export const mount = vueLifecycles.mount;
+export const unmount = vueLifecycles.unmount;
